e629eaf6da15506d5451b10d4db64646fbcc05cc
w.antar <w.antar@romulus.live>
2025-08-05 18:18:10 +0100

    Initial commit

---
+++ src/main/java/wael_project/transaction_file_bridge/TransactionFileBridgeApplication.java
@@ -0,0 +1,13 @@
+package wael_project.transaction_file_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFileBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionFileBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wael_project/transaction_file_bridge/TransactionFileBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package wael_project.transaction_file_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionFileBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
